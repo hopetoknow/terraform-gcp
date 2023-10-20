50e8c3f9f388cf147a1a056c7a5645921622c96c
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-10-20 21:18:39 +0300

    Replace google_compute_instance with google_compute_instance_template

--- resources.tf
+++ resources.tf
@@ -1,20 +1,19 @@
-resource "google_compute_instance" "app" {
-  name         = var.app_instance_parameters.name
-  machine_type = var.app_instance_parameters.machine_type
-  zone         = var.app_instance_parameters.zone
+resource "google_compute_instance_template" "app" {
+  name = var.app_instance_template_parameters.name
+  machine_type = var.app_instance_template_parameters.machine_type
 
-  boot_disk {
-    initialize_params {
-      image = var.app_instance_parameters.image
-    }
+  disk {
+    source_image = var.app_instance_template_parameters.image
   }
 
   network_interface {
-    network = var.app_instance_parameters.network
-    access_config {}
+    network = var.app_instance_template_parameters.network
   }
 
   metadata = {
-    ssh-keys = "${var.app_instance_parameters.ssh_user}:${file(var.app_instance_parameters.public_ssh_key_file_path)}"
+    ssh-keys = "${var.app_instance_template_parameters.ssh_user}:${file(var.app_instance_template_parameters.public_ssh_key_file_path)}"
   }
+
+  region = var.gcp_region
+  tags = ["allow-health-check"]
 }
