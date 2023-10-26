c1cf6e819dea7ca736aad88a508f20d9be228223
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-10-26 22:24:56 +0300

    Add instance_group and firewall rules

--- resources.tf
+++ resources.tf
@@ -1,30 +1,30 @@
 resource "google_compute_instance" "app" {
-  name         = var.app_instance_parameters.name
-  machine_type = var.app_instance_parameters.machine_type
-  zone         = var.app_instance_parameters.zone
+  name         = var.instance_parameters.name
+  machine_type = var.instance_parameters.machine_type
+  zone         = var.instance_parameters.zone
 
   boot_disk {
     initialize_params {
-      image = var.app_instance_parameters.image
+      image = var.instance_parameters.image
     }
   }
 
   network_interface {
-    network = var.app_instance_parameters.network
+    network = var.instance_parameters.network
     access_config {}
   }
 
   metadata = {
-    ssh-keys = "${var.app_instance_parameters.ssh_user}:${file(var.app_instance_parameters.public_ssh_key_file_path)}"
+    ssh-keys = "${var.instance_parameters.ssh_user}:${file(var.instance_parameters.public_ssh_key_file_path)}"
   }
 
-  tags = var.app_instance_parameters.tags
+  tags = var.instance_parameters.tags
 
   provisioner "remote-exec" {
     connection {
-      type        = var.app_instance_parameters.connection_type
-      user = var.app_instance_parameters.connection_user
-      private_key = file(var.app_instance_parameters.private_ssh_key_file_path)
+      type        = var.instance_parameters.connection_type
+      user = var.instance_parameters.connection_user
+      private_key = file(var.instance_parameters.private_ssh_key_file_path)
       host     = self.network_interface.0.access_config.0.nat_ip
     }
 
@@ -32,6 +32,51 @@ resource "google_compute_instance" "app" {
   }
 
   provisioner "local-exec" {
-    command = "ANSIBLE_SSH_ARGS='-o StrictHostKeyChecking=no' ansible-playbook -i '${google_compute_instance.app.network_interface.0.access_config.0.nat_ip},' test.yml --private-key=${var.app_instance_parameters.private_ssh_key_file_path}"
+    command = "ANSIBLE_SSH_ARGS='-o StrictHostKeyChecking=no' ansible-playbook -i '${google_compute_instance.app.network_interface.0.access_config.0.nat_ip},' app.yml --private-key=${var.instance_parameters.private_ssh_key_file_path}"
+    working_dir = "${path.module}/ansible" 
+  }
+}
+
+resource "google_compute_instance_group" "app" {
+  name        = var.instance_group_parameters.name
+  description = var.instance_group_parameters.description
+
+  instances = [
+    google_compute_instance.app.id
+  ]
+
+  named_port {
+    name = var.instance_group_parameters.named_port_name
+    port = var.instance_group_parameters.named_port
+  }
+
+  zone = var.instance_group_parameters.zone
+}
+
+resource "google_compute_firewall" "app" {
+  name          = var.app_firewall_parameters.name
+  direction     = var.app_firewall_parameters.direction
+  network       = var.app_firewall_parameters.network
+  priority      = var.app_firewall_parameters.priority
+  source_ranges = var.app_firewall_parameters.source_ranges
+  target_tags   = var.app_firewall_parameters.target_tags
+
+  allow {
+    ports    = var.app_firewall_parameters.allow_ports
+    protocol = var.app_firewall_parameters.allow_protocol
+  }
+}
+
+resource "google_compute_firewall" "health_check" {
+  name          = var.health_check_firewall_parameters.name
+  direction     = var.health_check_firewall_parameters.direction
+  network       = var.health_check_firewall_parameters.network
+  priority      = var.health_check_firewall_parameters.priority
+  source_ranges = var.health_check_firewall_parameters.source_ranges
+  target_tags   = var.health_check_firewall_parameters.target_tags
+  
+  allow {
+    ports    = var.health_check_firewall_parameters.allow_ports
+    protocol = var.health_check_firewall_parameters.allow_protocol
   }
 }
--- variables.tf
+++ variables.tf
@@ -13,8 +13,8 @@ variable "gcp_region" {
   description = "Google Cloud region for resources."
 }
 
-variable "app_instance_parameters" {
-  description = "Parameters for the app instance"
+variable "instance_parameters" {
+  description = "Parameters for the instance"
   type = object({
     name         = string # Name of the instance
     machine_type = string # Machine type for the instance
@@ -27,5 +27,44 @@ variable "app_instance_parameters" {
     connection_type = string # Type of connection
     connection_user = string # User for remote access
     tags = list(string) # Tags for the instance
+  })
+}
+
+variable "instance_group_parameters" {
+  description = "Parameters for the instance group"
+  type = object({
+    name        = string # Name of the instance group
+    description = string # Description of the instance group
+    zone        = string # Zone for the instance group
+    named_port_name = string # Name of the named port
+    named_port = string # Port number for the named port
+  })
+}
+
+variable "app_firewall_parameters" {
+  description = "Parameters for the app firewall"
+  type = object({
+    name          = string # Name of the firewall rule
+    direction     = string # Direction of traffic (e.g., INGRESS or EGRESS)
+    network       = string # Network for the firewall rule
+    priority      = number # Priority of the firewall rule
+    source_ranges = list(string) # List of source IP ranges
+    target_tags   = list(string) # List of target tags
+    allow_ports = list(string) # List of allowed ports
+    allow_protocol = string # Protocol for the allowed traffic
+  })
+}
+
+variable "health_check_firewall_parameters" {
+  description = "Parameters for the health check firewall"
+  type = object({
+    name          = string # Name of the firewall rule
+    direction     = string # Direction of traffic (e.g., INGRESS or EGRESS)
+    network       = string # Network for the firewall rule
+    priority      = number # Priority of the firewall rule
+    source_ranges = list(string) # List of source IP ranges
+    target_tags   = list(string) # List of target tags
+    allow_ports = list(string) # List of allowed ports
+    allow_protocol = string # Protocol for the allowed traffic
   })
 }
